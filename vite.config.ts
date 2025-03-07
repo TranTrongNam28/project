967241c262860c5066e9756968e6938f3c85de85
bin282001 <nam.197pm09478@vanlanguni.vn>
2025-03-07 23:48:34 +0700

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ["com-ga-phu-yen-hien.onrender.com"]
+  }
 });
